021ec8845ac5f9801fec9327a08e5edb5388e6f2
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-02 11:29:56 +0300

    Typo fixes in comments

--- src/index.js
+++ src/index.js
@@ -15,22 +15,17 @@ import { OAuthStrategy, InternalOAuthError } from 'passport-oauth';
  *   - `consumerKey`     identifies client to Twitter
  *   - `consumerSecret`  secret used to establish ownership of the consumer key
  *
- * Examples:
- *
- *     passport.use(new TwitterTokenStrategy({
- *         consumerKey: '123-456-789',
- *         consumerSecret: 'shhh-its-a-secret'
- *       },
- *       function(token, tokenSecret, profile, done) {
- *         User.findOrCreate(..., function (err, user) {
- *           done(err, user);
- *         });
- *       }
- *     ));
- *
  * @param {Object} options
  * @param {Function} verify
- * @api public
+ * @example
+ * passport.use(new TwitterTokenStrategy({
+ *   consumerKey: '123456789',
+ *   consumerSecret: 'shhh-its-a-secret'
+ * }), function(token, tokenSecret, profile, done) {
+ *   User.findOrCreate({twitterId: profile.id}, function(error, user) {
+ *     done(error, user);
+ *   });
+ * });
  */
 export default class TwitterTokenStrategy extends OAuthStrategy {
   constructor(_options, _verify) {
@@ -52,9 +47,7 @@ export default class TwitterTokenStrategy extends OAuthStrategy {
 
   /**
    * Authenticate request by delegating to Twitter using OAuth.
-   *
    * @param {Object} req
-   * @api protected
    */
   authenticate(req, options) {
     // Following the link back to the application is interpreted as an authentication failure
@@ -86,22 +79,10 @@ export default class TwitterTokenStrategy extends OAuthStrategy {
 
   /**
    * Retrieve user profile from Twitter.
-   *
-   * This function constructs a normalized profile, with the following properties:
-   *
-   *   - `id`        (equivalent to `user_id`)
-   *   - `username`  (equivalent to `screen_name`)
-   *
-   * Note that because Twitter supplies basic profile information in query
-   * parameters when redirecting back to the application, loading of Twitter
-   * profiles *does not* result in an additional HTTP request, when the
-   * `skipExtendedUserProfile` is enabled.
-   *
    * @param {String} token
    * @param {String} tokenSecret
    * @param {Object} params
    * @param {Function} done
-   * @api protected
    */
   userProfile(token, tokenSecret, params, done) {
     this._oauth.get(`https://api.twitter.com/1.1/users/show.json?user_id=${params.user_id}`, token, tokenSecret, (error, body, res) => {

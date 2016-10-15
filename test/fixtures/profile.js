0471c696640358e2eed4cba06bdc4e6557fff593
Munir Khakhi <khakhimunir@gmail.com>
2016-10-15 20:34:23 +0530

    Added support for fetch email

--- test/fixtures/profile.js
+++ test/fixtures/profile.js
@@ -3,5 +3,6 @@ export default JSON.stringify({
   id_str: '710474596655480832',
   screen_name: 'ghaiklor',
   name: 'Eugene Obrezkov',
-  profile_image_url_https: 'IMAGE_URL'
+  profile_image_url_https: 'IMAGE_URL',
+  email: 'ghaiklor@gmail.com'
 });
